f7e1535b61a3209613806d126ce80688ec008742
Scott Hendricks <shendricks@bensonhill.com>
2024-02-06 11:38:13 -0600

    updates to testin csv

--- Combine_UI/winter_file_format_testing.py
+++ Combine_UI/winter_file_format_testing.py
@@ -10,16 +10,22 @@ file_path = filedialog.askopenfilename()
 
 with open(file_path, "r") as fileInput:
 
-    reader_obj = csv.reader(fileInput)
+    # Skips the heading 
+    # Using next() method 
+     
+    header = pandas.read_csv(fileInput,index_col=0, nrows=0).columns.tolist()
+    print(header)
+    csvFile = pandas.read_csv(fileInput, sep=";", decimal=",")
+    print(csvFile)
 
-    for row in reader_obj:
+
         
 
 
-    csvFile = pandas.read_csv(fileInput)
-    print(csvFile)
-    #need "Timestamp","ID_REC","Property_2_Value" to "Protein","Property_2_H" to "H", "Pro" 
-    csvFile = csvFile[['Timestamp','ID_REC',"Property_2_Value","Property_2_H","Property_2_S"]]
-    csvFile.rename(columns={'Property_2_Value': 'Protein', 'Property_2_H': 'H', "Property_2_S":"S"}, inplace=True)
-    csvFile_rev = csvFile[::-1].reset_index(drop=True)
-    formatted_panda = csvFile_rev
+    # csvFile = pandas.read_csv(fileInput)
+    # print(csvFile)
+    # #need "Timestamp","ID_REC","Property_2_Value" to "Protein","Property_2_H" to "H", "Pro" 
+    # csvFile = csvFile[['Timestamp','ID_REC',"Property_2_Value","Property_2_H","Property_2_S"]]
+    # csvFile.rename(columns={'Property_2_Value': 'Protein', 'Property_2_H': 'H', "Property_2_S":"S"}, inplace=True)
+    # csvFile_rev = csvFile[::-1].reset_index(drop=True)
+    # formatted_panda = csvFile_rev
